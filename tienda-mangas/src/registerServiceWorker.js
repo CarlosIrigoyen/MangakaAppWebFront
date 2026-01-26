ea3b769bce59cf11b511ae28f375d8cc99185c4d
Irigoyen Carlos Damian <142269298+CarlosIrigoyen@users.noreply.github.com>
2026-01-26 16:21:58 -0300

    Add service worker registration function

---
+++ tienda-mangas/src/registerServiceWorker.js
@@ -0,0 +1,14 @@
+export function registerServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(reg => {
+          console.log('✅ Service Worker registrado', reg);
+        })
+        .catch(err => {
+          console.error('❌ Error registrando SW', err);
+        });
+    });
+  }
+}
